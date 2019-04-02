8e1299b0d58209fb9d74903b433172476a66f545
DhayanandBaskar <dhayanand.baskar@gmail.com>
2019-04-02 23:15:57 +0530

    Adds linked list

---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,68 @@
+package linkedList;
+
+/**
+ * Time complexity
+ * linkFirst, linkLast - O(1)
+ * linkBetween - O(1)
+ */
+
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+
+    public class Node {
+        private int data;
+        private Node next;
+        private Node prev;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+    }
+
+    public void linkFirst(int data) {
+        Node old = first;
+        first = new Node(data);
+        if(last == null) {
+            last = first;
+        } else {
+            first.next = old;
+            old.prev = first;
+        }
+    }
+
+    public void linkLast(int data) {
+        Node secLast = last;
+        last = new Node(data);
+        secLast.next = last;
+        last.prev = secLast;
+    }
+
+    public void linkBetween(int data, Node node) {
+        Node temp = new Node(data);
+        temp.prev = node;
+        temp.next = node.next;
+        node.next = temp;
+    }
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+}
---
+++ test/linkedList/LinkedListTest.java
@@ -0,0 +1,49 @@
+package linkedList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    @Test
+    public void shouldBeAbleToAddElementsAndIterateThroughList() {
+        LinkedList list = constructLinkedList();
+
+        LinkedList.Node node = list.getFirst();
+        StringBuilder br = new StringBuilder();
+        int count = 1;
+        while(node != null) {
+            if(node.getData()%2 == 1 && count++ < 10)
+                list.linkBetween(node.getData()+1, node);
+            br.append(node.getData() + " ");
+            node = node.getNext();
+        }
+        System.out.println(br.toString().trim());
+        assertEquals("5 6 7 8 9 10 11 12", br.toString().trim());
+    }
+
+    @Test
+    public void shouldBeAbleToTraverseTheLinkedListInReverseOrder() {
+        LinkedList list = constructLinkedList();
+
+        LinkedList.Node node = list.getLast();
+        StringBuilder br = new StringBuilder();
+        while(node != null) {
+            br.append(node.getData() + " ");
+            node = node.getPrev();
+        }
+
+        System.out.println(br.toString().trim());
+        assertEquals("11 9 7 5", br.toString().trim());
+    }
+
+    private LinkedList constructLinkedList() {
+        LinkedList list = new LinkedList();
+        list.linkFirst(7);
+        list.linkFirst(5);
+        list.linkLast(9);
+        list.linkLast(11);
+        return list;
+    }
+}
