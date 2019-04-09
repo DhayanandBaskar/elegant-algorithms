848226792f1ebdaaacdf6a2227f6d26affe3dfaa
DhayanandBaskar <dhayanand.baskar@gmail.com>
2019-04-09 22:27:56 +0530

    Adds solution to reverse a singly linked list problem

--- src/dataStructures/linkedList/ReverseASinglyLinkedList.java
+++ src/dataStructures/linkedList/ReverseASinglyLinkedList.java
@@ -1,4 +1,25 @@
 package dataStructures.linkedList;
 
+/**
+ * Time complexity - O(n)
+ */
+
 public class ReverseASinglyLinkedList {
+    public LinkedList.Node reverse(LinkedList.Node node) {
+        if (node == null) return null;
+
+        LinkedList.Node prev = null;
+        LinkedList.Node current = node;
+        LinkedList.Node next = current.next;
+
+        while (current != null) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (current != null) {
+                next = current.next;
+            }
+        }
+        return prev;
+    }
 }
--- test/dataStructures/linkedList/ReverseASinglyLinkedListTest.java
+++ test/dataStructures/linkedList/ReverseASinglyLinkedListTest.java
@@ -1,5 +1,31 @@
+package dataStructures.linkedList;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ReverseASinglyLinkedListTest {
 
+    @Test
+    public void shouldReverseASinglyLinkedList() {
+        ReverseASinglyLinkedList reverser = new ReverseASinglyLinkedList();
+        LinkedList.Node node = reverser.reverse(constructLinkedList().getFirst());
+        StringBuilder br = new StringBuilder();
+        while (node != null) {
+            br.append(node.getData() + " ");
+            node = node.getNext();
+        }
+
+        System.out.println(br.toString().trim());
+        assertEquals("11 10 9 8", br.toString().trim());
+    }
+
+    private LinkedList constructLinkedList() {
+        LinkedList list = new LinkedList();
+        list.linkLast(8);
+        list.linkLast(9);
+        list.linkLast(10);
+        list.linkLast(11);
+        return list;
+    }
 }
